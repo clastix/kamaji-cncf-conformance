a71b8365484f87e95a9dc61e427ecf1879ff65dc
bsctl <adriano@clastix.io>
2022-09-23 15:30:55 +0200

    update path for test results

--- sonobuoy/scripts/cncf-conformance.bash
+++ sonobuoy/scripts/cncf-conformance.bash
@@ -9,5 +9,6 @@ cd $(dirname $0)
 source "../../kamaji/kamaji.env"
 
 sonobuoy --kubeconfig=/tmp/$TENANT_NAME.kubeconfig run --mode=certified-conformance --wait=180
-sonobuoy --kubeconfig=/tmp/$TENANT_NAME.kubeconfig retrieve --filename /tmp/$TENANT_NAME-sonobuoy-results.tar.gz
+sonobuoy --kubeconfig=/tmp/$TENANT_NAME.kubeconfig retrieve --filename $TENANT_NAME-sonobuoy-results.tar.gz
 sonobuoy --kubeconfig=/tmp/$TENANT_NAME.kubeconfig delete
+mv $TENANT_NAME-sonobuoy-results.tar.gz /tmp
